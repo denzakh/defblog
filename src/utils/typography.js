29b27a5580648c0db309a712759435fb7e389aaf
denzakh <den.zakh@gmail.com>
2019-09-22 15:19:15 +0300

    theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,8 +1,16 @@
 import Typography from 'typography'
-import Wordpress2016 from 'typography-theme-wordpress-2016'
+import t1 from 'typography-theme-wordpress-2016'
 
-Wordpress2016.overrideThemeStyles = () => {
-  baseFontSize: '26px'
+
+t1.bodyFontFamily = ["PT Sans", "Arial", "sans-serif"]
+t1.baseLineHeight = "1.45"
+t1.headerFontFamily = ["PT Sans", "Arial", "sans-serif"]
+t1.headerWeight = "600"
+t1.headerLineHeight = "1.15"
+
+
+t1.overrideThemeStyles = () => {
+
   return {
     'a.gatsby-resp-image-link': {
       boxShadow: `none`,
@@ -10,9 +18,13 @@ Wordpress2016.overrideThemeStyles = () => {
   }
 }
 
-delete Wordpress2016.googleFonts
+// delete t1.googleFonts
 
-const typography = new Typography(Wordpress2016)
+const typography = new Typography(t1)
+
+//
+
+console.dir(typography.options);
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
